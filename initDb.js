aec5bed7253e649c4b1639e803f2c5d6d4df3d2b
Petros DR <pdouros@newpharm.gr>
2026-03-26 10:52:28 +0200

    test2

--- initDb.js
+++ initDb.js
@@ -1,21 +1,38 @@
 const pool = require('./db');
 
+const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 const initDb = async () => {
-  try {
-    const createTableQuery = `
-      CREATE TABLE IF NOT EXISTS contacts (
-        id SERIAL PRIMARY KEY,
-        first_name VARCHAR(100) NOT NULL,
-        last_name VARCHAR(100) NOT NULL,
-        phone VARCHAR(20) NOT NULL
-      );
-    `;
+  let retries = 5;
 
-    await pool.query(createTableQuery);
+  while (retries) {
+    try {
+      console.log('⏳ Trying to connect to DB...');
 
-    console.log('✅ Table "contacts" is ready (created if not exists)');
-  } catch (err) {
-    console.error('❌ Error creating table:', err);
+      const createTableQuery = `
+        CREATE TABLE IF NOT EXISTS contacts (
+          id SERIAL PRIMARY KEY,
+          first_name VARCHAR(100) NOT NULL,
+          last_name VARCHAR(100) NOT NULL,
+          phone VARCHAR(20) NOT NULL
+        );
+      `;
+
+      await pool.query(createTableQuery);
+
+      console.log('✅ Table "contacts" is ready (created if not exists)');
+      break;
+
+    } catch (err) {
+      console.error('❌ DB not ready, retrying...', err.code);
+
+      retries--;
+      await wait(5000);
+    }
+  }
+
+  if (!retries) {
+    console.error('❌ Could not connect to DB after retries');
   }
 };
 
